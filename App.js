39eb7323f90a54d50836b7b6132565ac6ee4367e
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2022-05-26 16:48:01 +0530

    Added react-helmet library and the Google AdSense code in the App.js

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Helmet } from 'react-helmet';
 import Home from './src/Home';
 
 export default function App() {
-  return (
+  return (<>
+    <Helmet>
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9543408536781579"
+        crossorigin="anonymous"></script>
+    </Helmet>
     <View style={styles.container}>
       <Text style={styles.titleText}>Free time in a Week</Text>
       <Home />
       <StatusBar style="auto" />
     </View>
+  </>
   );
 }
 
