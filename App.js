53b0b3bf3dc4eddee2233bb1a312d50358642c2f
Avikalp Kumar Gupta <avikalpg@users.noreply.github.com>
2022-05-27 18:42:58 +0530

    Adding Google Adsense code and the Privacy policy (V0) (#9)
    * Added react-helmet library and the Google AdSense code in the App.js
    * AdSense: added a PrivacyPolicy component with serves a Markdown file describing our privacy policy

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Helmet } from 'react-helmet';
 import Home from './src/Home';
+import PrivacyPolicy from './src/PrivacyPolicy';
 
 export default function App() {
-  return (
+  return (<>
+    <Helmet>
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9543408536781579"
+        crossorigin="anonymous"></script>
+    </Helmet>
     <View style={styles.container}>
       <Text style={styles.titleText}>Free time in a Week</Text>
       <Home />
+      <PrivacyPolicy />
       <StatusBar style="auto" />
     </View>
+  </>
   );
 }
 
