660bc0350ddeeee4480f146e60ae0f0421774476
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2025-01-17 22:05:58 +0530

    minor: add full-stop at the end of the default recommendation statements

--- src/TimeUtilizationSuggestions.js
+++ src/TimeUtilizationSuggestions.js
@@ -84,10 +84,10 @@ export function TimeUtilizationSuggestions(props) {
     };
 
     const getDefaultSuggestion = () => {
-        if (hoursRemaining > 40) return "You can take up another full time job";
-        if (hoursRemaining > 20) return "You can probably start a side hustle or a part-time job like Uber driving";
-        if (hoursRemaining > 5) return "You can probably schedule dedicated time to spend with your family, friends or hobbies";
-        return "Congratulations, you are making the most of your life. But if you are aren't happy, you should probably reassess the importance and priority of some of these activities and remove/outsource the ones that don't give you a lot of joy";
+        if (hoursRemaining > 40) return "You can take up another full time job.";
+        if (hoursRemaining > 20) return "You can probably start a side hustle or a part-time job like Uber driving.";
+        if (hoursRemaining > 5) return "You can probably schedule dedicated time to spend with your family, friends or hobbies.";
+        return "Congratulations, you are making the most of your life. But if you are aren't happy, you should probably reassess the importance and priority of some of these activities and remove/outsource the ones that don't give you a lot of joy.";
     }
 
     return (
